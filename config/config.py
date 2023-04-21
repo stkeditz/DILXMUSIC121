3322c7ace8140d68224fa00455c94c80ffc8958d
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-21 10:18:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5140509190").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "2107529793").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
